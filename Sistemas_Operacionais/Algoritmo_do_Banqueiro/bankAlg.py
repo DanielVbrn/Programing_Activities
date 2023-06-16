ffdcc0184b9928203d3efca449730b6aeb69db3a
DanielVbrn <you@example.com>
2023-06-16 12:58:39 -0300

    add function insecureSystem

--- Sistemas_Operacionais/Algoritmo_do_Banqueiro/bankAlg.py
+++ Sistemas_Operacionais/Algoritmo_do_Banqueiro/bankAlg.py
@@ -1,47 +1,50 @@
-import matplotlib.pyplot as plt
-
-
 def main():
-    menu = "Escolha qual o cenário do sistema: "
-    menu += "\n1- Seguro"
-    menu += "\n2- Inseguro"
-    option = input()
+    menu = "---------(ALGORÍTMO DO BANQUEIRO)-----------"
+    menu += "\nEscolha qual o cenário do sistema: "
+    menu += "\n1 - Seguro"
+    menu += "\n"
+    menu += "\n2 - Inseguro"
+    menu += "\n"    
+    menu += "\n"
+    print(menu)
 
-
-    processes = [0, 1, 2, 3, 4]  # Identificadores dos processos
-    available = [3, 3, 2]  # Recursos disponíveis
-    available2 = [1, 0, 0]  # Recursos disponíveis
+    processes = [0, 1, 2, 3]  # Identificadores dos processos
+    available = [7, 4, 3]  # Recursos disponíveis
+    available2 = [7, 1, 2]  # Recursos disponíveis
     max_resources = [
         [4, 5, 3],
         [3, 2, 2],
         [9, 0, 2],
-        [2, 2, 2],
-        [4, 3, 3]
+        [6, 2, 2],
     ]  # Máximo de recursos necessários para cada processo
     allocated = [
         [0, 1, 0],
         [2, 0, 0],
         [3, 0, 2],
         [2, 1, 1],
-        [0, 0, 2]
     ]  # Recursos alocados para cada processo
         
-    if(option == 1):
-        secureSystem(processes, available, max_resources, allocated)
-    elif(option ==2):
-        insecureSystem(processes, available2, max_resources, allocated)
+    option = input("Opção: ")
+    if(option == "1"):
+        is_safe, history = secureSystem(processes, available, max_resources, allocated)
+        if is_safe:
+            print("O estado do sistema é seguro.")
+        else:
+            print("O estado do sistema é inseguro.")
+        print("Histórico de alocação de recursos:")
+        for state in history:
+            print(state)    
+    elif(option == "2"):
+        is_safe, history, deadlock_processes = insecureSystem(processes, available2, max_resources, allocated)
+        if is_safe:
+            print("O estado do sistema é seguro.")
+        else:
+            print(f"O estado do sistema é inseguro. Deadlock detectado nos processos: {deadlock_processes}.")
+        print("Histórico de alocação de recursos:")
+        for state in history:
+            print(state)
         
 
-    # Verificando se o sistema está em um estado seguro e obtendo o histórico
-    safe, history = secureSystem(processes, available, max_resources, allocated)
-    if safe:
-        print("O estado do sistema é seguro.")
-    else:
-        print("O estado do sistema é inseguro.")
-
-    # Gerando os gráficos
-
-    plt.figure(figsize=(10, 5))
 
     # Plotando a utilização dos recursos e o status de cada processo ao longo do tempo
 
@@ -80,31 +83,33 @@ def secureSystem(processes, available, max_resources, allocated):
         process_status = ["Executando" if finish[i] else "Bloqueado" for i in range(num_processes)]
 
         # Adicionando o estado atual à lista de histórico
-        history.append((work.copy(), process_status.copy()))
+        history.append(work.copy())
 
         # Verificando se todos os processos foram concluídos
         if not found:
             break
 
-    
-    
-    # Verificando se todos os processos foram concluídos em segurança
-    return all(finish), history
+    # Verificando se o sistema está em um estado seguro e obtendo o histórico
+    safe = all(finish)
+    return safe, history
 
-def insecureSystem(processes, available, max_resources, allocated):
+def insecureSystem(processes, available2, max_resources, allocated):
     num_processes = len(processes)
-    num_resources = len(available)
+    num_resources = len(available2)
 
     # Inicializando as listas de trabalho, alocados e concluídos
-    work = available.copy()
+    work = available2.copy()
     finish = [False] * num_processes
 
     # Verificando se um processo pode ser executado em segurança
     history = []
-    process_status = [False] * num_processes  # Status do processo (Executando ou Bloqueado)
+    deadlock_detected = False  # Flag para indicar se deadlock foi detectado
+    deadlock_processes = []  # Lista de processos onde ocorreu o deadlock
 
     while True:
         found = False
+        process_status = [False] * num_processes  # Status do processo (Executando ou Bloqueado)
+
         for i in range(num_processes):
             if not finish[i]:
                 can_execute = True
@@ -125,37 +130,16 @@ def insecureSystem(processes, available, max_resources, allocated):
         process_status = ["Executando" if finish[i] else "Bloqueado" for i in range(num_processes)]
 
         # Adicionando o estado atual à lista de histórico
-        history.append((work.copy(), process_status.copy()))
+        history.append(work.copy())
 
         # Verificando se todos os processos foram concluídos
         if not found:
+            deadlock_detected = True
+            deadlock_processes = [i for i in range(num_processes) if not finish[i]]
             break
 
-    N_resources = len(available)
-    num_steps = len(history)
-    for resource in range(N_resources):
-        plt.subplot(N_resources + 1, 1, resource + 1)
-        plt.step(range(num_steps), [state[0][resource] for state in history], where='post')
-        plt.ylabel(f"Recurso {resource + 1} (disponível)")
-        plt.ylim(min(available) - 1, max(available) + 1)
-        plt.xticks(range(num_steps), range(num_steps))
-        plt.xlabel("Passos")
-
-    # Plotando o status de cada processo ao longo do tempo
-    plt.subplot(N_resources + 1, 1, N_resources + 1)
-    for i in range(len(processes)):
-        plt.step(range(num_steps), [1 if state[1][i] == "Executando" else 0 for state in history], where='post',
-                 label=f"Processo {i}")
-    plt.ylim(-0.1, 1.1)
-    plt.ylabel("Status do Processo")
-    plt.yticks([0, 1], ["Bloqueado", "Executando"])
-    plt.xticks(range(num_steps), range(num_steps))
-    plt.xlabel("Passos")
-    plt.legend(loc="upper right")
-
-    plt.tight_layout()
-    plt.show()
-    # Verificando se todos os processos foram concluídos em segurança
-    return all(finish), history
+    # Verificando se o sistema está em um estado seguro e obtendo o histórico
+    safe = not deadlock_detected
+    return safe, history, deadlock_processes
 
 main()
