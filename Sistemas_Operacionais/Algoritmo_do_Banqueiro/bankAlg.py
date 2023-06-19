8a550cdff9f5950ed83d58757200c1d03cccc2ac
Daniel Vitor <102341547+DanielVbrn@users.noreply.github.com>
2023-06-19 15:45:03 -0300

    Update bankAlg.py

--- Sistemas_Operacionais/Algoritmo_do_Banqueiro/bankAlg.py
+++ Sistemas_Operacionais/Algoritmo_do_Banqueiro/bankAlg.py
@@ -16,13 +16,13 @@ def main():
         [3, 2, 2],
         [9, 0, 2],
         [6, 2, 2],
-    ]  # Máximo de recursos necessários para cada processo
+    ]  
     allocated = [
         [0, 1, 0],
         [2, 0, 0],
         [3, 0, 2],
         [2, 1, 1],
-    ]  # Recursos alocados para cada processo
+    ]  
         
     option = input("Opção: ")
     if(option == "1"):
