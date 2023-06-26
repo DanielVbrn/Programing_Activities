e5f1a476f3d361078f6d5292185e50352fb87383
DanielVbrn <danielvbrn@gmail.com>
2023-06-25 21:38:46 -0300

    update folder DS2

--- Estrutura_de_dados_2/Grafos/15_7Ordemtopologica.cpp
+++ Estrutura_de_dados_2/Grafos/15_7Ordemtopologica.cpp
@@ -1,44 +1,59 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 class Grafo {
 private:
-    int numVertices;
+    unordered_map<string, int> indices;
+    vector<string> disciplinas;
     vector<vector<int>> adjacencias;
+    vector<int> grauEntrada;
 
 public:
-    Grafo(int numVertices) : numVertices(numVertices), adjacencias(numVertices) {}
+    void adicionarAresta(string origem, string destino) {
+        // Verifica se o vértice já foi adicionado
+        if (indices.count(origem) == 0) {
+            indices[origem] = disciplinas.size();
+            disciplinas.push_back(origem);
+            adjacencias.emplace_back();
+            grauEntrada.push_back(0);
+        }
+        if (indices.count(destino) == 0) {
+            indices[destino] = disciplinas.size();
+            disciplinas.push_back(destino);
+            adjacencias.emplace_back();
+            grauEntrada.push_back(0);
+        }
 
-    void adicionarAresta(int origem, int destino) {
-        adjacencias[origem].push_back(destino);
+        // Adiciona a aresta no grafo
+        int origemIndex = indices[origem];
+        int destinoIndex = indices[destino];
+        adjacencias[origemIndex].push_back(destinoIndex);
+        grauEntrada[destinoIndex]++;
     }
 
     vector<int> ordenacaoTopologica() {
-        vector<int> grauEntrada(numVertices, 0);
-        for (int i = 0; i < numVertices; i++) {
-            for (int vizinho : adjacencias[i]) {
-                grauEntrada[vizinho]++;
-            }
-        }
-
-        vector<int> ordem;
         queue<int> fila;
+        vector<int> ordemTopologica;
 
-        for (int i = 0; i < numVertices; i++) {
+        // Encontra os vértices iniciais com grau de entrada zero
+        for (int i = 0; i < disciplinas.size(); i++) {
             if (grauEntrada[i] == 0) {
                 fila.push(i);
             }
         }
 
+        // Realiza a ordenação topológica
         while (!fila.empty()) {
-            int no = fila.front();
+            int vertice = fila.front();
             fila.pop();
-            ordem.push_back(no);
+            ordemTopologica.push_back(vertice);
 
-            for (int vizinho : adjacencias[no]) {
+            for (int vizinho : adjacencias[vertice]) {
                 grauEntrada[vizinho]--;
                 if (grauEntrada[vizinho] == 0) {
                     fila.push(vizinho);
@@ -46,34 +61,73 @@ public:
             }
         }
 
-        if (ordem.size() != numVertices) {
-            cout << "O grafo contém um ciclo!" << endl;
-            return vector<int>();
+        // Verifica se todos os vértices foram visitados
+        if (ordemTopologica.size() != disciplinas.size()) {
+            ordemTopologica.clear(); // Retorna um vetor vazio em caso de ciclo
         }
 
-        return ordem;
+        return ordemTopologica;
+    }
+
+    void imprimirOrdemTopologica() {
+        vector<int> ordem = ordenacaoTopologica();
+
+        if (!ordem.empty()) {
+            cout << "Ordem topológica das disciplinas:" << endl;
+            for (int i : ordem) {
+                cout << i << ": " << disciplinas[i] << endl;
+            }
+        }
+    }
+
+    void disciplinasMesmoSemestre() {
+        vector<int> ordem = ordenacaoTopologica();
+
+        if (!ordem.empty()) {
+            unordered_set<int> semestreAtual;
+            int semestre = 1;
+
+            cout << "Disciplinas no mesmo semestre:" << endl;
+
+            for (int i : ordem) {
+                semestreAtual.insert(i);
+
+                bool temDependencias = false;
+                for (int vizinho : adjacencias[i]) {
+                    if (semestreAtual.count(vizinho) == 0) {
+                        temDependencias = true;
+                        break;
+                    }
+                }
+
+                if (!temDependencias) {
+                    cout << i << ": " << disciplinas[i] << endl;
+                    semestreAtual.clear();
+                    semestre++;
+                }
+            }
+        }
     }
 };
 
 int main() {
-    Grafo grafo(6);
-    grafo.adicionarAresta(0, 1);
-    grafo.adicionarAresta(0, 2);
-    grafo.adicionarAresta(1, 3);
-    grafo.adicionarAresta(2, 3);
-    grafo.adicionarAresta(2, 4);
-    grafo.adicionarAresta(3, 5);
-    grafo.adicionarAresta(4, 5);
+    Grafo grafo;
 
-    vector<int> ordem = grafo.ordenacaoTopologica();
+    // Adiciona as arestas com os pré-requisitos
+    grafo.adicionarAresta("Arquitetura de Computadores", "Algoritmos");
+    grafo.adicionarAresta("Matematica Discreta", "Algoritmos");
+    grafo.adicionarAresta("Algoritmos", "Linguagem de Programacao");
+    grafo.adicionarAresta("Linguagem de Programacao", "Estrutura de Dados");
+    grafo.adicionarAresta("Linguagem de Programacao", "Programacao Orientada a Objetos");
+    grafo.adicionarAresta("Estrutura de Dados", "Calculo Numerico Computacional");
+    grafo.adicionarAresta("Estrutura de Dados", "Banco de Dados");
+    grafo.adicionarAresta("Matematica Discreta", "Calculo 1");
+    grafo.adicionarAresta("Calculo 1", "Calculo II");
+    grafo.adicionarAresta("Calculo II", "Calculo Numerico Computacional");
 
-    if (!ordem.empty()) {
-        cout << "Ordem topológica: ";
-        for (int no : ordem) {
-            cout << no << " ";
-        }
-        cout << endl;
-    }
+    grafo.imprimirOrdemTopologica();
+    cout << endl;
+    grafo.disciplinasMesmoSemestre();
 
     return 0;
 }
